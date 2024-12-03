46546b96c4fc45bf3d703ad2303cc49c56bc1e66
Jake Otte <jake.otte@tannersecurity.com>
2024-12-03 14:58:03 -0700

    initial

--- whos_up.py
+++ whos_up.py
@@ -2,7 +2,9 @@ import argparse
 import requests
 import ipaddress
 from time import sleep
+from lxml.html import fromstring
 import random
+import bs4
 from colorama import Fore
 from colorama import Style
 from colorama import init as colorama_init
@@ -42,7 +44,7 @@ parser.add_argument('-p', help='Port list to test. Default is 80,443.', default=
 parser.add_argument('--no-random', help='Don\'t randomly select hosts from subnets.', action="store_true")
 parser.add_argument('-s', help='Sleep in seconds. Default is 5s.', default=5, type=int, metavar='[num]')
 parser.add_argument('-j', help='Jitter in seconds. Default is 2s.', default=2, type=int, metavar='[num]')
-parser.add_argument('-ua', help='User-agent for requests. Default is from a random pool.', metavar='"Mozilla 1.x"')
+parser.add_argument('-ua', help='User-agent for requests. Default is Windows/Mozilla.', default='Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:133.0) Gecko/20100101 Firefox/133.0', metavar='"Mozilla 1.x"')
 
 parser.add_argument('-o', help='Output file for alive hosts. Defaults to web-hosts.txt.', default='web-hosts.txt',  metavar='alive.txt')
 
@@ -56,13 +58,24 @@ settings_blob = f"""{Style.BRIGHT}{Fore.MAGENTA}[*]{Style.RESET_ALL} Scan settin
 - User-agent: {args.ua if args.ua else 'Random (default)'}
 >> Press Enter to start scan..."""
 
-def all_elements_in(list_a, list_b):
-    """Check if all elements of list_a are in list_b."""
-    return all(item in list_b for item in list_a)
+def printResponse(response):
+    html = bs4.BeautifulSoup(response.text)
+    print(f"    [ Status : {response.status_code} ]")
+    try:
+        server = response.headers['Server']
+    except:
+        server = "???"
+    print(f"    [ Server : {server} ]")
+    print(f"    [ Title : {html.title.text if html.title else "???"} ]")
+
 
 def main():
     print(settings_blob)
     ports = args.p.split(',')
+
+    
+
+
     input()
 
     # CIDR Subnet
@@ -82,25 +95,34 @@ def main():
         for i in targets:
             while target in scanned:
                 target = random.choice(targets)
-            print(f"\r\\--> {target}", end="")
-            # Try HTTP and then check exceptions
+            
+            
             try:
+                # HTTP
                 # "Sleep" is just our timeout
-                response = requests.get(f"http://{target}", timeout=2)
+                response = requests.get(f"http://{target}", timeout=(1 if (args.s == 0) else args.s), verify=False)
                 print(f"{Style.BRIGHT}{Fore.GREEN}[+]{Style.RESET_ALL} http://{Style.RESET_ALL}{target} is up!")
+                printResponse(response)
+
             except requests.exceptions.SSLError:
+                # HTTPS
                 response = requests.get(f"https://{target}", timeout=10, verify=False)
-                print(f"{Style.BRIGHT}{Fore.GREEN}[+]{Style.RESET_ALL} https://{target} is up!")
+                print(f"{Style.BRIGHT}{Fore.GREEN}[+]{Style.RESET_ALL} https://{Style.RESET_ALL}{target} is up!")
+                printResponse(response)
+
             except requests.exceptions.SSLError:
+                # HTTPS with additional SSL error
                 print(f"{Style.BRIGHT}{Fore.GREEN}[+]{Style.RESET_ALL} https://{target} has an SSL error.")
+
             except requests.exceptions.ReadTimeout:
+                # Timeouts after SSL
                 print(f"{Style.BRIGHT}{Fore.GREEN}[+]{Style.RESET_ALL} https://{target} timed out, but is likely up.")
+
             except:
-                # print(f"{target} is down.")
                 pass
             
             # "Jitter" is inbetween jobs
-            sleep(args.s + random.randint(0,args.j))
+            sleep(random.randint(0,args.j))
             scanned.append(target)
         
 
